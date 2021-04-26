4ae47346578dc6da06be2b11b230009d21e8b263
Amsiam <amsiam990@gmail.com>
2021-04-26 16:03:38 +0600

    adding media settings

--- mainproject/urls.py
+++ mainproject/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 import debug_toolbar
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
@@ -28,6 +29,7 @@ urlpatterns = [
 
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
